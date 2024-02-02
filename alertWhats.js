dacd06772a4b2152cb8e915d4628e92c104ca284
Nleroux <code.leroux@gmail.com>
2024-02-02 01:27:19 -0300

    Arreglos varios

--- alertWhats.js
+++ alertWhats.js
@@ -1,9 +1,14 @@
-document.getElementById('whatsappIcon').addEventListener('click', function() {
+var whatsappIcons = document.getElementsByClassName("whatsappIcon");
 
-  Swal.fire({
-    title: 'Chatea con nosotros',
-    html: '<a class="linkWappExpand" target="_blank" href="https://api.whatsapp.com/send/?phone=543513473383&text=Hola+Nexum,+quería+consultar+...&type=phone_number&app_absent=0">Gerardo Rey</a> <br> <br>  <a class="linkWappExpand" target="_blank" href="https://api.whatsapp.com/send/?phone=543513609840&text=Hola+Nexum,+quería+consultar+...&type=phone_number&app_absent=0">Gastón Piazza</a>',
-    confirmButtonColor: '#207981',
-    color: '#2b9aa1'
+for (var i = 0; i < whatsappIcons.length; i++) {
+  whatsappIcons[i].addEventListener("click", function () {
+    Swal.fire({
+      title: "Chatea con nosotros",
+      html:
+        '<a class="linkWappExpand" target="_blank" href="https://api.whatsapp.com/send/?phone=543513473383&text=Hola+Nexum,+quería+consultar+...&type=phone_number&app_absent=0">Gerardo Rey</a>' +
+        ' <br> <br>  <a class="linkWappExpand" target="_blank" href="https://api.whatsapp.com/send/?phone=543513609840&text=Hola+Nexum,+quería+consultar+...&type=phone_number&app_absent=0">Gastón Piazza</a>',
+      confirmButtonColor: "#207981",
+      color: "#2b9aa1",
+    });
   });
-});
+}
