717f041602cfabcdcaa939dd380b78193b48d724
Nleroux <code.leroux@gmail.com>
2024-02-02 01:39:24 -0300

    Correccion mensaje whatsapp

--- alertWhats.js
+++ alertWhats.js
@@ -5,8 +5,8 @@ for (var i = 0; i < whatsappIcons.length; i++) {
     Swal.fire({
       title: "Chatea con nosotros",
       html:
-        '<a class="linkWappExpand" target="_blank" href="https://api.whatsapp.com/send/?phone=543513473383&text=Hola+Nexum,+quería+consultar+...&type=phone_number&app_absent=0">Gerardo Rey</a>' +
-        ' <br> <br>  <a class="linkWappExpand" target="_blank" href="https://api.whatsapp.com/send/?phone=543513609840&text=Hola+Nexum,+quería+consultar+...&type=phone_number&app_absent=0">Gastón Piazza</a>',
+        '<a class="linkWappExpand" target="_blank" href="https://api.whatsapp.com/send/?phone=543513473383&text=Gracias+por+contactarte+con+Nexum+te+responderemos+a+la+brevedad...&type=phone_number&app_absent=0">Gerardo Rey</a>' +
+        ' <br> <br>  <a class="linkWappExpand" target="_blank" href="https://api.whatsapp.com/send/?phone=543513609840&text=Gracias+por+contactarte+con+Nexum+te+responderemos+a+la+brevedad...&type=phone_number&app_absent=0">Gastón Piazza</a>',
       confirmButtonColor: "#207981",
       color: "#2b9aa1",
     });
